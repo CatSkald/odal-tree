e10d7b2f863a1b0e1671b98040a48e51e57f2815
CatSkald <CatSkald@users.noreply.github.com>
2017-04-25 21:13:21 +0200

    Refactored tests. Added buttons.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,32 +1,48 @@
-import { TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core/core';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let debugElement: DebugElement;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    component = fixture.nativeElement;
+    debugElement = fixture.debugElement;
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(debugElement.componentInstance).toBeTruthy();
   }));
 
-  it(`should have as title 'Welcome to Odal Tree!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Welcome to Odal Tree!');
+  it(`should have as title 'Welcome to Odal Tree!`, async(() => {
+    expect(debugElement.componentInstance.title).toEqual('Welcome to Odal Tree!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Odal Tree!');
+    expect(debugElement.nativeElement.querySelector('h1').textContent)
+      .toContain('Welcome to Odal Tree!');
+  }));
+
+  it('should render button New', async(() => {
+    const button = debugElement.queryAll(By.css('button'))
+      .find(it => it.nativeElement.textContent === 'New');
+
+    expect(button).toBeTruthy();
+  }));
+
+  it('should render button Open', async(() => {
+    const button = debugElement.queryAll(By.css('button'))
+      .find(it => it.nativeElement.textContent === 'Open');
+
+    expect(button).toBeTruthy();
   }));
 });
